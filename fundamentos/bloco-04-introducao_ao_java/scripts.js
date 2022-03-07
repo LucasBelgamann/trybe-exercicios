a4171017c00ba5a85a84d3e25e24edbd95f7ea10
Lucas Agostinho <lucasbelgamann@hotmail.com>
2022-03-07 18:58:50 -0300

    para fixar, tipos primitivos

--- fundamentos/bloco-04-introducao_ao_java/scripts.js
+++ fundamentos/bloco-04-introducao_ao_java/scripts.js
@@ -1,10 +1,34 @@
-const myName = "Lucas";
-const birthCity = "Pinhais";
-let birthYear = 2001;
-birthYear = 2030;
-birthCity = "Curitiba";
+// const myName = "Lucas";
+// const birthCity = "Pinhais";
+// let birthYear = 2001;
+// birthYear = 2030;
+// birthCity = "Curitiba";
 
-console.log(myName, birthCity, birthYear);
-console.log(birthYear);
-console.log(birthCity);
+// console.log(myName, birthCity, birthYear);
+// console.log(birthYear);
+// console.log(birthCity);
 
+let patientId = '50';
+let isEnrolled = true;
+const patientInfo = {
+  firstName: 'Ana',
+  lastName: 'Santos',
+};
+const patientEmail = 'ana@email.com';
+
+console.log(typeof patientId);
+console.log(typeof isEnrolled);
+console.log(typeof patientInfo);
+console.log(typeof firstName);
+console.log(typeof lastName);
+console.log(typeof patientEmail);
+console.log(typeof patientAge)
+
+const base = 5;
+let height = 8;
+const area = (3 * 8);
+const perimeter = base + height + area;
+
+console.log(base, height, area);
+console.log(area);
+console.log(perimeter);
