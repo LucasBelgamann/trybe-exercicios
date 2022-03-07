93fd60b634a20c2556d79697e7bfe15973ca8970
Lucas Agostinho <lucasbelgamann@hotmail.com>
2022-03-07 18:15:23 -0300

    Para fixar, variaveis

---
+++ fundamentos/bloco-04-introducao_ao_java/scripts.js
@@ -0,0 +1,10 @@
+const myName = "Lucas";
+const birthCity = "Pinhais";
+let birthYear = 2001;
+birthYear = 2030;
+birthCity = "Curitiba";
+
+console.log(myName, birthCity, birthYear);
+console.log(birthYear);
+console.log(birthCity);
+
