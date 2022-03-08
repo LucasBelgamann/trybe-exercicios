840aaf0f0b2c3faeb3a59dbb3469f72b50f0ca19
Lucas Agostinho <lucasbelgamann@hotmail.com>
2022-03-08 16:15:51 -0300

    Para fixar, Arrays, for

---
+++ fundamentos/bloco-04-introducao_ao_java/Dia2.js
@@ -0,0 +1,21 @@
+
+// let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+// let indexOfPortfolio = menu.indexOf('Portfólio');
+// menu.push('Contato');
+
+// console.log(indexOfPortfolio);
+// console.log(menu);
+
+let groceryList = ['Arroz', 'Feijão', 'Alface', 'Melancia'];
+
+for (let index = 0; index < groceryList.length; index++) {
+    console.log(groceryList[index]);
+}
+
+let names = ['João', 'Maria', 'Antônio', 'Margarida'];
+
+for (nomes of names) {
+    console.log(nomes);
+}
+
+
