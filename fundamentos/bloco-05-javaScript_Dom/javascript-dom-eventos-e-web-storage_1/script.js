164812126a28f468ae02b60ba4c78d2e95fdebc6
Lucas Agostinho <lucasbelgamann@hotmail.com>
2022-03-14 20:17:24 -0300

    Exercicios 5.1

---
+++ fundamentos/bloco-05-javaScript_Dom/javascript-dom-eventos-e-web-storage_1/script.js
@@ -0,0 +1,42 @@
+// 1. Crie uma função que mude o texto na tag <p> para uma descrição de como você se vê daqui a 2 anos. (Não gaste tempo pensando no texto e sim realizando o exercício)
+function mudarTexto() {
+  document.getElementById("text").innerText =
+    "Me vejo trabalhando na área de desenvolvimento web";
+}
+mudarTexto();
+// 2. Crie uma função que mude a cor do quadrado amarelo para o verde da Trybe (rgb(76,164,109)).
+let cor = "rgb(76, 164, 109)";
+
+function mudarFundo(cor) {
+  document.getElementsByClassName("main-content")[0].style.backgroundColor =
+    cor;
+}
+
+mudarFundo(cor);
+
+// 3. Crie uma função que mude a cor do quadrado vermelho para branco.
+let corQuadrado = "#ffffff";
+function mudarCorQuadrado(corQuadrado) {
+  document.getElementsByClassName("center-content")[0].style.backgroundColor =
+    corQuadrado;
+}
+mudarCorQuadrado(corQuadrado);
+
+// 4. Crie uma função que corrija o texto da tag <h1>.
+
+function corrigirTexto() {
+  document.getElementById("text2").innerText = "Desenvolvimento web";
+}
+corrigirTexto();
+
+// 5. Crie uma função que modifique todo o texto da tag <p> para maiúsculo.
+function modificarMaiusculo() {
+  document.getElementsByTagName("p")[0].innerText =
+    "Me vejo trabalhando na área de desenvolvimento web".toUpperCase();
+}
+modificarMaiusculo();
+// 6. Crie uma função que exiba o conteúdo de todas as tags <p> no console.
+function exibicao() {
+  document.getElementsByTagName("p");
+}
+console.log(exibicao());
---
+++ fundamentos/bloco-05-javaScript_Dom/script.js
@@ -0,0 +1,8 @@
+// const paragraph = document.getElementById("paragraph");
+// paragraph.style.color = "red";
+
+// const title = document.getElementById("page-title");
+// title.innerText = "Harry potter";
+
+// const secondParagraph = document.getElementById("second-paragraph");
+// secondParagraph.innerText = "O melhor filme de fantasia";
