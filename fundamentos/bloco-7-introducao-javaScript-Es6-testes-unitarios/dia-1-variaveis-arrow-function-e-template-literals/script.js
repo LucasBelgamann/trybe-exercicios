aecbd3f45b1d35094886120534dc50496486e1d2
LucasBelgamann <lucasbelgamann@hotmail.com>
2022-04-01 18:45:20 -0300

    primeira parte, exercicio 2

--- fundamentos/bloco-7-introducao-javaScript-Es6-testes-unitarios/dia-1-variaveis-arrow-function-e-template-literals/script.js
+++ fundamentos/bloco-7-introducao-javaScript-Es6-testes-unitarios/dia-1-variaveis-arrow-function-e-template-literals/script.js
@@ -1,4 +1,4 @@
-//Exercício 1
+// Primeira parte: Exercício 1
 const testingScope = (escopo) => {
   if (escopo === true) {
     let ifScope = `Não devo ser utilizada fora do meu escopo (if)`;
@@ -11,3 +11,10 @@ const testingScope = (escopo) => {
 };
 testingScope(true);
 
+//Exercício 2
+const oddsAndEvens = [13, 3, 4, 10, 7, 2];
+
+// Primeira parte: Seu código aqui.
+const ordenaNumeros = (oddsAndEvens) => oddsAndEvens.sort();
+
+console.log(`Os numeros ${ordenaNumeros(oddsAndEvens)} se encontraram ordenados de forma crescente!`);
