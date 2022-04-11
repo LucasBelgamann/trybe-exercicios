67d840c828b84296b21495927f8946b67cf9a6a5
LucasBelgamann <lucasbelgamann@hotmail.com>
2022-04-11 15:38:40 -0300

    exercicio 2

---
+++ fundamentos/bloco-8-higher-Functions/dia-3-map-e-filter/exercicio2.js
@@ -0,0 +1,80 @@
+const books = [
+  {
+    id: 1,
+    name: 'As Crônicas de Gelo e Fogo',
+    genre: 'Fantasia',
+    author: {
+      name: 'George R. R. Martin',
+      birthYear: 1948,
+      age: 43,
+    },
+    releaseYear: 1991,
+  },
+  {
+    id: 2,
+    name: 'O Senhor dos Anéis',
+    genre: 'Fantasia',
+    author: {
+      name: 'J. R. R. Tolkien',
+      birthYear: 1892,
+      age: 62,
+    },
+    releaseYear: 1954,
+  },
+  {
+    id: 3,
+    name: 'Fundação',
+    genre: 'Ficção Científica',
+    author: {
+      name: 'Isaac Asimov',
+      birthYear: 1920,
+      age: 31,
+    },
+    releaseYear: 1951,
+  },
+  {
+    id: 4,
+    name: 'Duna',
+    genre: 'Ficção Científica',
+    author: {
+      name: 'Frank Herbert',
+      birthYear: 1920,
+      age: 45,
+    },
+    releaseYear: 1965,
+  },
+  {
+    id: 5,
+    name: 'A Coisa',
+    genre: 'Terror',
+    author: {
+      name: 'Stephen King',
+      birthYear: 1947,
+      age: 39,
+    },
+    releaseYear: 1986,
+  },
+  {
+    id: 6,
+    name: 'O Chamado de Cthulhu',
+    genre: 'Terror',
+    author: {
+      name: 'H. P. Lovecraft',
+      birthYear: 1890,
+      age: 38,
+    },
+    releaseYear: 1928,
+  },
+];
+let nameAndAge;
+
+nameAndAge = books.map((elemento) => {
+    return {
+        age: elemento.author.age,
+        author: elemento.author.name,
+    }
+})
+nameAndAge.sort((a, b) => a.age - b.age);
+
+
+console.log(nameAndAge);
