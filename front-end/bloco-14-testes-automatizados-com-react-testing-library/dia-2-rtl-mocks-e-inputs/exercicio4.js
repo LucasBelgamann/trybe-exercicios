b1e595433d36addf95700ec457d480593dce3f7d
LucasBelgamann <lucasbelgamann@hotmail.com>
2022-06-02 21:08:17 -0300

    Exercicio 4

---
+++ front-end/bloco-14-testes-automatizados-com-react-testing-library/dia-2-rtl-mocks-e-inputs/exercicio4.js
@@ -0,0 +1,13 @@
+const caixaAlta = (string) => string.toUpperCase();
+
+
+const firstWord = (string) => string.charAt();
+
+
+const concatena = (string, word) => `${string} ${word}`;
+
+module.exports = {
+    caixaAlta,
+    firstWord,
+    concatena
+};
